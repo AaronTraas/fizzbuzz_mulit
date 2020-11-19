6b2da882adde7d0de4d353bd6eb3e88e5dee6f6a
Aaron Traas <aaron@traas.org>
2020-11-19 16:45:35 -0500

    Added Java implementations

---
+++ java/fizzbuzz.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+
+class FizzBuzz {
+
+    private final Map<Integer, String> replacementMap;
+
+    public FizzBuzz() {
+        replacementMap = Map.of(
+                3, "Fizz",
+                5, "Buzz"
+        );
+    }
+
+    public final String fizzbuzzer(int num) {
+        StringBuilder out = new StringBuilder();
+
+        for (Map.Entry<Integer, String> entry : replacementMap.entrySet()) {
+            if (num % entry.getKey() == 0) {
+                out.append(entry.getValue());
+            }
+        }
+
+        if (out.length() == 0) {
+            out.append(num);
+        } 
+
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+
+        for (int num = 1; num <= 100; num++) 
+        {
+            System.out.println(fb.fizzbuzzer(num));
+        }
+        
+    }
+}
---
+++ java/fizzbuzz_fun.java
@@ -0,0 +1,32 @@
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+class FizzBuzzFunctional {
+
+    public static final String fizzbuzzer(int num, Map<Integer, String> replacementMap) {
+        return Optional.of(Optional.ofNullable(replacementMap)
+                .map(Map::entrySet)
+                .orElse(Collections.emptySet())
+                .stream()
+                .filter(entry -> num % entry.getKey() == 0)
+                .map(Map.Entry::getValue)
+                .collect(Collectors.joining()))
+            .filter(Predicate.not(String::isBlank))
+            .orElse(String.valueOf(num));
+    }
+
+    public static void main(String[] args) {
+        final Map<Integer, String> replacementMap = Map.of(
+            3, "Fizz",
+            5, "Buzz"
+        );
+
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(FizzBuzz.fizzbuzzer(num, replacementMap));
+        }
+        
+    }
+}
